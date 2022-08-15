85a28973b675fd95461383e82e73b78d86b25a29
Chrissy Hsu <chrissy@ChrissydeMacBook-Pro.local>
2022-08-15 12:50:56 +0800

    add cass

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Layout.module.css'
+import styles from '../style/Layout.module.css'
 import Nav from './Nav';
 
 const Layout = ({children}: any) => {
