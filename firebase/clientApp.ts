104ce4f383457a227036ed4c4a990861c3d1d4db
chrissy93881209 <chrissy93881209@gmail.com>
2024-07-23 16:27:17 +0800

    add: env

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,20 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+require("dotenv").config();
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBtM06wq0Y_KPB1F-jQnWtLgiL9hA036RY",
-  authDomain: "todo-391e5.firebaseapp.com",
-  projectId: "todo-391e5",
-  storageBucket: "todo-391e5.appspot.com",
-  messagingSenderId: "542436459686",
-  appId: "1:542436459686:web:c68c9526e6ce5a161aa067"
-};
+import { initializeApp } from "firebase/app";
+import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
